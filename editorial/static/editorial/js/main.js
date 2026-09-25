96cc65b3b8f04a00d144c2bd7cf1bc8289612579
DABOURGOU Larba <dabourgoularba@gmail.com>
2026-09-25 16:09:42 +0000

    Ajout et configuration du fichier JavaScript main.js

---
+++ editorial/static/editorial/js/main.js
@@ -0,0 +1,4 @@
+// On attend que la page soit complètement chargée
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Le fichier JS de la plateforme éditoriale est bien chargé !");
+});
